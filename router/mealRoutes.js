8d04985acbd44ea8d87add3702ed3dad2d917680
Brunoarcofitnessapp <brunoarcofitness@gmail.com>
2025-01-29 22:29:55 -0300

    Update mealRoutes.js

--- router/mealRoutes.js
+++ router/mealRoutes.js
@@ -15,13 +15,13 @@ mealRouter.get("/", (req, res) => {
 // Rutas para las operaciones de meals
 mealRouter.post(
     "/createMeal",
-    multer.imageuploadmiddleware.single("image"), // Middleware para procesar la imagen
+    multer.imageuploadmiddleware.single("photo"), // Middleware para procesar la imagen (clave: "photo")
     MealController.createMeal
 );
 
 mealRouter.post(
     "/uploadMealImage/:id",
-    multer.imageuploadmiddleware.single("image"), // Middleware para procesar la imagen
+    multer.imageuploadmiddleware.single("photo"), // Middleware para procesar la imagen (clave: "photo")
     MealController.uploadMealImage
 );
 
