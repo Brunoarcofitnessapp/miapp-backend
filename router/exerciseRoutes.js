67c9c7190063facfe7ec50d35119648eb9190a2d
Brunoarcofitnessapp <brunoarcofitness@gmail.com>
2025-01-30 15:18:24 -0300

    Update exerciseRoutes.js

--- router/exerciseRoutes.js
+++ router/exerciseRoutes.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const ExerciseController = require('../controllers/exerciseController')
+const express = require("express");
+const ExerciseController = require("../controllers/exerciseController");
 
 const exerciseRouter = express.Router();
 
-exerciseRouter.post('/createExercise',ExerciseController.exerciseimageuploadmiddleware,ExerciseController.createExercise)
-exerciseRouter.post('/removeuserfromexercise/:id',ExerciseController.removeuserfromexercise)
-exerciseRouter.get('/getAllExercise',ExerciseController.getAllExercises)
-// exerciseRouter.post('/getExercises/:id',ExerciseController.getExercises)
-exerciseRouter.get(
-    "/getAllUserExercises/:id",
-    ExerciseController.getAllUserExercises
-  );
+// Crear un ejercicio con imagen
+exerciseRouter.post(
+  "/createExercise",
+  ExerciseController.exerciseimageuploadmiddleware,
+  ExerciseController.createExercise
+);
 
-module.exports = exerciseRouter
+// Obtener todos los ejercicios
+exerciseRouter.get("/getAllExercise", ExerciseController.getAllExercises);
+
+// Obtener todos los ejercicios de un usuario
+exerciseRouter.get(
+  "/getAllUserExercises/:id",
+  ExerciseController.getAllUserExercises
+);
+
+// Remover un usuario de un ejercicio
+exerciseRouter.post(
+  "/removeuserfromexercise/:exerciseid/:userid",
+  ExerciseController.removeuserfromexercise
+);
+
+module.exports = exerciseRouter;
