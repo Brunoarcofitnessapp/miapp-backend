94f1aeb6a1db19c91c747fc978eacb0660f9c538
Brunoarcofitnessapp <brunoarcofitness@gmail.com>
2025-01-29 21:07:37 -0300

    Update userRoutes.js

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,34 +1,30 @@
 const express = require('express');
 const AuthController = require('../controllers/authController');
 const UserController = require('../controllers/userController');
-const { imageuploadmiddleware } = require('../util/multer'); // Importamos multer
 
 const userRouter = express.Router();
 
-// Ruta base de prueba
+// Ruta de prueba para verificar que el endpoint esté funcionando
 userRouter.get('/', (req, res) => {
     res.status(200).json({
         message: "User endpoint is working!",
     });
 });
 
-// Endpoints
-userRouter.post('/login', AuthController.LoginwithEmailandPassword);
+// Rutas de autenticación
 userRouter.post('/signup', AuthController.signup);
+userRouter.post('/login', AuthController.LoginwithEmailandPassword);
+
+// Rutas de usuario
 userRouter.post('/updateMeasures/:id', UserController.updateUserMeasurables);
-
-// Integrar multer para procesar imágenes
-userRouter.post(
-    '/uploadimage/:id',
-    imageuploadmiddleware.single('image'), // Middleware para manejar una sola imagen
-    UserController.uploadUserBodyImage
-);
-
+userRouter.post('/uploadimage/:id', UserController.uploadUserBodyImage);
 userRouter.post('/deleteimages/:id', UserController.deleteImageOfUserBody);
 userRouter.get('/updateMeasureRecords/:id/:measure', UserController.updateUserBodyMeasureRecord);
 userRouter.post('/updateusername', AuthController.AuthMiddleware, UserController.updateusername);
 userRouter.post('/updateuserimage', AuthController.AuthMiddleware, UserController.updateuserimage);
 userRouter.post('/updateuserpassword', AuthController.AuthMiddleware, UserController.changePassword);
+
+// Rutas de recuperación de contraseña
 userRouter.post('/forgotpassword', AuthController.forgotPassword);
 userRouter.post('/matchotp', AuthController.matchOtp);
 userRouter.post('/resetpassword', AuthController.resetpassword);
